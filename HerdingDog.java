57b1a23094b0948e14c70dad9459748775b71f34
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-06-08 21:54:54 +0800

    inheritance

---
+++ HerdingDog.java
@@ -0,0 +1,10 @@
+
+public class HerdingDog extends Dog {
+    HerdingDog(String name, int age, String breed) {
+        super(name, age, breed);
+    }
+
+    public void herd() {
+        System.out.println("Herding...");
+    }
+}
